f204807146201ea4fcecb558c4e7546cb56b3151
erickrocha87 <ericksaggi31@gmail.com>
2024-11-19 11:16:19 -0300

    Find JPA

--- java-jpamaven/src/main/java/application/App.java
+++ java-jpamaven/src/main/java/application/App.java
@@ -8,19 +8,14 @@ import dominio.Pessoa;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Pessoa p1 = new Pessoa(null, "Erick Rocha", "Ericksaggi31@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Erick Saggiorato", "Ericksaggi@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Erick", "ErickRsaggi@gmail.com");
-        
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
-        EntityManager em = emf.createEntityManager(); //fazendo isso automaticamente vamos ter a conexão com o banco de dados
 
-        em.getTransaction().begin();
-        em.persist(p1); //persist -> pega um objeto e salvo no bd
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit(); //confirma as alterações
-        System.out.println("Pronto");
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager(); // fazendo isso automaticamente vamos ter a conexão com o banco de // dados
+        Pessoa p = em.find(Pessoa.class, 1);
+
+        System.out.println(p);
+        System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 }
